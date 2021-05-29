b4a4174c244893afeea83ee49ed354574a9911a8
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-05-29 15:37:41 +0300

    api constants added

--- SensorDataApp/Commons/APIConstants/ApiConstants.swift
+++ SensorDataApp/Commons/APIConstants/ApiConstants.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+final class ApiConstants {
+  static let getAllDevicesURL = baseURL() + "/devices"
+
+  static func getDeviceDetail(deviceID: String) -> String {
+    return getAllDevicesURL + deviceID
+  }
+
+  static func getDeviceReadings(deviceID: String) -> String {
+    return getDeviceDetail(deviceID: deviceID) + "/readings"
+  }
+
+}
+
+extension ApiConstants {
+  static func baseURL() -> String {
+    return "https://canary-homework-test.herokuapp.com"
+  }
+}
