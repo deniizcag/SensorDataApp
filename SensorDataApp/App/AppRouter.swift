4db3360d0a6bdaa6a8009e9378bf69f271c2f796
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-05-30 14:43:07 +0300

    start from router

--- SensorDataApp/App/AppRouter.swift
+++ SensorDataApp/App/AppRouter.swift
@@ -5,4 +5,19 @@
 //  Created by DenizCagilligecit on 29.05.2021.
 //
 
-import Foundation
+import UIKit
+
+
+final class AppRouter {
+    let window: UIWindow
+    init() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+    }
+    func start() {
+      let viewController = DeviceListBuilder.make()
+      viewController.viewModel =  DeviceListViewModel(service: app.service, coreDataStack: CoreDataStack(modelName: "SensorDataApp"))
+      let rootController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = rootController
+        window.makeKeyAndVisible()
+    }
+}
