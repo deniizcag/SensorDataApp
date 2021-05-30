9e0b15108e8f04efdf6607f3b247d33b0ff2e9df
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-05-30 14:45:03 +0300

    fetch data from core data

--- SensorDataApp/Scenes/DeviceList/DeviceListViewModel.swift
+++ SensorDataApp/Scenes/DeviceList/DeviceListViewModel.swift
@@ -6,27 +6,108 @@
 //
 
 import Foundation
-
+import CoreData
 final class DeviceListViewModel: DeviceListVMProtocol {
   var delegate: DeviceListVMDelegate?
   var service: FetchDeviceListServiceProtocol
-  init(service: FetchDeviceListServiceProtocol) {
+  var coreDataStack: CoreDataStack
+  var fetchRequest: NSFetchRequest<Device>!
+  var devices: [Device] = []
+
+  init(service: FetchDeviceListServiceProtocol,coreDataStack: CoreDataStack) {
     self.service = service
+    self.coreDataStack = coreDataStack
   }
 
   func fetch() {
+    deleteAllData("Device")
+    self.notify(.setLoading(true))
     service.fetchDeviceList(url: ApiConstants.getAllDevicesURL) { (result) in
       switch result {
       case .success(let devices):
-        print(devices)
+        self.saveLocally(devices: devices)
+
       case .failure(let error):
         print(error)
+        self.fetchFromCoreData()
       }
     }
 
   }
 
+  func saveLocally(devices: [AnyObject]) {
+    let deviceEntity = NSEntityDescription.entity(forEntityName: "Device", in: coreDataStack.managedContext)!
+    for device in devices {
+      if let id = device["id"] as? Int16,
+         let name = device["name"] as? String,
+         !someEntityExists(id: id)
+         {
+        let deviceObject = Device(entity: deviceEntity, insertInto: coreDataStack.managedContext)
+        deviceObject.id = id
+        deviceObject.name = name
+
+      }
+
+    }
+    coreDataStack.saveContext()
+
+    fetchFromCoreData()
+
+
+
+  }
+  func deleteAllData(_ entity:String) {
+      let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+      fetchRequest.returnsObjectsAsFaults = false
+      do {
+          let results = try coreDataStack.managedContext.fetch(fetchRequest)
+          for object in results {
+              guard let objectData = object as? NSManagedObject else {continue}
+            coreDataStack.managedContext.delete(objectData)
+          }
+        try coreDataStack.managedContext.save()
+      } catch let error {
+          print("Detele all data in \(entity) error :", error)
+      }
+  }
+  func someEntityExists(id: Int16) -> Bool {
+      let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Device")
+      fetchRequest.predicate = NSPredicate(format: "id = %d", id)
+
+      var results: [NSManagedObject] = []
+
+      do {
+        results = try coreDataStack.managedContext.fetch(fetchRequest)
+      }
+      catch {
+          print("error executing fetch request: \(error)")
+      }
+
+      return results.count > 0
+  }
+
   func selectDevice(at index: Int) {
+
+  }
+
+  func notify(_ outputs: DeviceListVMOutput) {
+    delegate?.handleViewModelOutputs(outputs)
+
+  }
+
+
+
+  func fetchFromCoreData() {
+    fetchRequest = Device.fetchRequest()
+    do {
+      devices = try coreDataStack.managedContext.fetch(fetchRequest)
+      self.notify(.showDeviceList(devices))
+      self.notify(.setLoading(false))
+    }
+    catch {
+      self.notify(.showDeviceList([]))
+    }
+
 
   }
 
