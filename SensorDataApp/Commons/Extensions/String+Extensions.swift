b5623f1a4530812e12b0adc96421ea087a82f7bd
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-05-31 19:50:19 +0300

    string extensions added

--- SensorDataApp/Commons/Extensions/String+Extensions.swift
+++ SensorDataApp/Commons/Extensions/String+Extensions.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+extension String {
+
+  func formatDate() -> String {
+    let stringData = self
+    let dateFormatter = DateFormatter()
+    let tempLocale = dateFormatter.locale // save locale temporarily
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    let date = dateFormatter.date(from: stringData)!
+    dateFormatter.dateFormat = "HH:mm:ss dd/MM/yyyy"
+    dateFormatter.locale = tempLocale // reset the locale
+    dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+    let dateString = dateFormatter.string(from: date)
+    return dateString
+  }
+
+  func cellFormat() -> String {
+    return self.split(separator: "_").joined(separator: " ").capitalized
+  }
+
+}
